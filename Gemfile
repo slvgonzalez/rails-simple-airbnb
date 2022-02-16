4b9b011b09324e33ee1305571f257ad223a56320
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-16 16:30:05 +0000

    gems installed

--- Gemfile
+++ Gemfile
@@ -21,9 +21,12 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+24.times do
+    puts "start creating a flat"
+    Flat.create!(
+    name: 'Light & Spacious Garden Flat London',
+    address: Faker::Address.street_address,
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: rand(25..500),
+    number_of_guests: rand(1..8)
+    )
+    puts "finished creating flat #{flat.id}"
+  end
